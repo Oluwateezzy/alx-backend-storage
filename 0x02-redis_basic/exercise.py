28d174e1c9467e734b1d3aa3415b4ed440458777
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-02 13:26:47 +0100

    update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -61,19 +61,20 @@ def call_history(method: Callable) -> Callable:
 
 
 class Cache:
+    """ Cache """
     def __init__(self) -> None:
         """ Initialize """
         self._redis = redis.Redis()
         self._redis.flushdb()
-    
+
     @call_history
     @count_calls
-    def store(self, data: Union[str, bytes, int , float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """ store """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-    
+
     def get(self,
             key: str,
             fn: Callable = None,
@@ -81,11 +82,11 @@ class Cache:
         """ get method """
         data = self._redis.get(key)
         return fn(data) if fn is not None else data
-    
+
     def get_str(self, key: str):
         """ get string """
         return self.get(key, lambda x: x.decode('utf-8'))
-    
+
     def get_int(self, key: str):
         """ get integer """
         return self.get(key, lambda x: int(x))
